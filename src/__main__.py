9f266c0a4c823a42cfc653ab12a1a6fbf7e8a8f1
João Paulo Carvalho <jjpaulo2@protonmail.com>
2025-08-25 15:14:20 -0300

    feat: project sketch

---
+++ src/__main__.py
@@ -0,0 +1,6 @@
+from src.app import client
+from src.settings import DISCORD_TOKEN
+
+
+if __name__ == '__main__':
+    client.run(DISCORD_TOKEN)
---
+++ src/settings.py
@@ -0,0 +1,5 @@
+from os import getenv
+
+
+DISCORD_TOKEN = getenv("DISCORD_TOKEN", "")
+DISCORD_GUILD_ID = getenv("DISCORD_GUILD_ID", "")
