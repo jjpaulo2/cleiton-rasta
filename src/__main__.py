f6eeec41c3c2d53b2505b00ecf8ed043b3e40c35
João Paulo Carvalho <jjpaulo2@protonmail.com>
2026-05-30 19:55:15 -0300

    fix: bot run

--- src/__main__.py
+++ src/__main__.py
@@ -1,6 +1,6 @@
-from src.app import get_bot
+from src.app import client
 from src.settings import DISCORD_TOKEN
 
 
 if __name__ == '__main__':
-    get_bot().run(DISCORD_TOKEN)
+    client.run(DISCORD_TOKEN)
