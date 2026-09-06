d8c8b5c27d2caf6094ee60c82224c670b7e5f577
João Paulo Carvalho <jjpaulo2@protonmail.com>
2026-09-06 13:34:26 -0300

    feat: allowing play audios without obligation os being on the channel

--- src/commands/audios.py
+++ src/commands/audios.py
@@ -1,4 +1,4 @@
-from discord import Interaction
+from discord import Interaction, VoiceChannel
 from discord.app_commands import Choice, Group, command, describe, choices, rename
 
 from structlog import get_logger
@@ -29,44 +29,58 @@ class AudiosCommands(Group):
 
     @command(name="tocar", description="Toca um efeito sonoro na sala de voz")
     @rename(audio="áudio")
+    @rename(channel="canal")
     @describe(audio="Selecione o efeito sonoro que deseja tocar.")
     @choices(audio=AUDIOS_CHOICES)
     async def play(
         self,
         interaction: Interaction,
         audio: Choice[str],
+        channel: VoiceChannel,
     ):
         self.logger.info(
             f"Pedindo para tocar efeito sonoro...",
             user=interaction.user.name,
             audio=audio.value,
+            channel=channel.name,
         )
 
         try:
             await interaction.response.defer(ephemeral=True)
 
-            if not interaction.user.voice:
-                await interaction.edit_original_response(content="❌ Você precisa estar em uma sala de voz!")
+            if 'lobby' not in channel.name.lower():
+                await interaction.edit_original_response(content="🔴 Só é possível tocar em um **lobby**!")
+                return
+            
+            if len(channel.members) < 1:
+                await interaction.edit_original_response(content="🔴 O canal precisa ter pelo menos **uma pessoa** conectada!")
                 return
 
             await interaction.edit_original_response(
-                content=f"⏳ Tocando ``{audio.value}``..."
+                content=f"⏳ Tocando ``{audio.value}`` em {channel.mention}..."
             )
             await self.audio.play(
-                channel=interaction.user.voice.channel,
+                channel=channel,
                 filename=audio.value
             )
             await interaction.edit_original_response(
-                content=f"✅ Efeito sonoro ``{audio.value}`` tocado com sucesso!"
+                content=f"✅ Efeito sonoro ``{audio.value}`` tocado com sucesso em {channel.mention}!"
+            )
+            self.logger.info(
+                f"Efeito sonoro tocado com sucesso!",
+                user=interaction.user.name,
+                channel=channel.name,
+                audio=audio.value
             )
 
         except Exception as e:
             self.logger.error(
                 f"Erro ao tentar tocar efeito sonoro!",
                 user=interaction.user.name,
+                channel=channel.name,
                 audio=audio.value,
                 error=str(e)
             )
             await interaction.edit_original_response(
-                content="❌ Ocorreu um erro ao tentar tocar o efeito sonoro."
+                content="🔴 Ocorreu um erro ao tentar tocar o efeito sonoro."
             )
