d6523392eea506f89f994e7148e4d67caff5e1a5
João Paulo Carvalho <jjpaulo2@protonmail.com>
2026-09-06 16:54:54 -0300

    feat: adding emoji to status

--- src/utils.py
+++ src/utils.py
@@ -3,5 +3,5 @@ from discord import Client, CustomActivity
 
 async def set_default_activity(client: Client):
     await client.change_presence(
-        activity=CustomActivity(name="Xingando caixistas na internet")
+        activity=CustomActivity(name="🤬 Xingando caixistas na internet")
     )
