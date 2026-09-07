56076aca7388452ecdbedad5701b4049cf3ac075
João Paulo Carvalho <jjpaulo2@protonmail.com>
2026-09-07 20:31:27 -0300

    feat: new audios

--- src/settings/audios.py
+++ src/settings/audios.py
@@ -20,3 +20,13 @@ AI_MEU_C = AudioEffect(
     title="🥵 Ai meu c...",
     filename="ai-meu-c.mp3"
 )
+
+WITCH_DOCTOR = AudioEffect(
+    title="🧙‍♂️ Ooh look at it go! (Witch Doctor)",
+    filename="witch-doctor.mp3"
+)
+
+OLHA_A_PEDRA = AudioEffect(
+    title="🪨 Olha a pedra!",
+    filename="olha-a-pedra.mp3"
+)
