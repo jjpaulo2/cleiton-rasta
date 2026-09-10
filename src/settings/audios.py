0b82719b82468de043a0d3f1baa1701df342bfd1
João Paulo Carvalho <jjpaulo2@protonmail.com>
2026-09-10 14:29:19 -0300

    refact: moving audios to proper folders

--- src/settings/audios.py
+++ src/settings/audios.py
@@ -3,17 +3,17 @@ from src.models import AudioEffect
 
 STONE_MASK = AudioEffect(
     title="🗿 Ooooh StoneMask...",
-    filename="stonemask.mp3"
+    filename="seu-loro/stonemask.mp3"
 )
 
 DIEGO = AudioEffect(
     title="🤤 Diegóóó...",
-    filename="diego.mp3"
+    filename="seu-loro/diego.mp3"
 )
 
 SEU_LORO = AudioEffect(
     title="🦜 Seu Loro...",
-    filename="seu-loro.mp3"
+    filename="kajuru/seu-loro.mp3"
 )
 
 BAPHOMET = AudioEffect(
@@ -33,20 +33,20 @@ AI_MEU_C = AudioEffect(
 
 OLHA_A_PEDRA = AudioEffect(
     title="🪨 Olha a pedra!",
-    filename="olha-a-pedra.mp3"
+    filename="cleiton-rasta/olha-a-pedra.mp3"
 )
 
 BOLSONARISTA = AudioEffect(
     title="🤤 Mija na minha boca mito!",
-    filename="bolsonarista.mp3"
+    filename="stonemask/bolsonarista.mp3"
 )
 
 HUMOR = AudioEffect(
     title="😂 Humor!",
-    filename="humor.mp3"
+    filename="lukita/humor.mp3"
 )
 
 VTMNC = AudioEffect(
     title="🤬 Vai tomar no cu!",
-    filename="vtmnc.mp3"
+    filename="lukita/vtmnc.mp3"
 )
